917d8d0a84786da42fb815444b1c0d1fd101e3e8
chakyunghee <datascience.ckh@gmail.com>
2022-04-21 14:17:03 +0900

    initial

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# SoyChai_nmt
+# nmt
+시스메틱 &amp; 패스트캠퍼스 기업연계 프로젝트 (NMT 팀)
+
+## Python
+    (버전)
+    python3.8
+    
+    (모듈)
+    requirements.txt 내 참고
+
+## 가상환경 venv 활용 방법
+
+    (Packages 업데이트)
+    sudo apt update && sudo apt upgrade -y
+    
+    (Python3.8 설치)
+    sudo apt install python3.8
+    (Linux Ubuntu 기준) /usr/bin/python3.8
+    
+    (venv 설치)
+    sudo apt install virtualenv
+    
+    (프로젝트로 이동)
+    cd ner
+    (venv 생성)
+    virtual venv --python==/usr/bin/python3.8
+    
+    (venv 실행)
+    . venv/bin/activate
+    
+    (python 확인)
+    which python
+
+## 파이썬 모듈 설치 (이 부분은 동일하셔야 합니다!!)
+    pip install -r requirements.txt
---
+++ teams/chakyunghee/experiments/simple_nmt/trainer.py
@@ -0,0 +1,243 @@
+import numpy as np
+import torch
+import torch.nn.utils as torch_utils
+from torch.cuda.amp import autocast
+from torch.cuda.amp import GradScaler
+
+from ignite.engine import Engine
+from ignite.engine import Events
+from ignite.metrics import RunningAverage
+
+VERBOSE_SILENT = 0
+VERBOSE_EPOCH_WISE = 1
+VERBOSE_BATCH_WISE = 2
+
+class MaximumLikelihoodEstimationEngine(Engine):
+
+    def __init__(self, func, model, crit, optimizer, lr_scheduler, config):
+        self.model = model
+        self.crit = crit
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.config =config
+
+        super().__init__(func)
+
+        self.best_loss = np.inf
+        self.scaler = GradScaler()
+
+    @staticmethod
+    def train(engine, mini_batch):          # process function(ignite에 등록 됨)
+        engine.model.train()                # gradient accumulation
+        if engine.state.iteration % engine.config.iteration_per_update == 1 or \
+            engine.config.iteration_per_update == 1:    # 매번 업뎃
+            if engine.state.iteration > 1:
+                engine.optimizer.zero_grad()
+
+        device = next(engine.model.parameter()).device
+        mini_batch.src = (mini_batch.src[0].to(device), mini_batch.src[1])
+        mini_batch.tgt = (mini_batch.tgt[0].to(device), mini_batch.tgt[1])
+
+        x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+
+        with autocast(not engine.config.off_autocast):  # AMP 쓸 것임
+            y_hat = engine.model(x, mini_batch.tgt[0][:, :-1])
+            loss = engine.crit(
+                y_hat.contiguous().view(-1, y_hat.size(-1)),
+                y.contiguous().view(-1)
+            )                                          # gradient accumulation했기 때문
+            backward_target = loss.div(y.size(0)).div(engine.config.iteration_per_update) # gradient descent 수행할 애.
+
+        if engine.config.gpu_id >= 0 and not engine.config.off_autocast:
+            engine.scaler.scale(backward_target).backward() # 안정적 backprop
+        else:
+            backward_target.backward()
+
+        word_count = int(mini_batch.tgt[1].sum())
+
+        if engine.state.iteration % engine.config.iteration_per_update == 0 and \
+            engine.state.iteration > 0:
+
+            torch_utils.clip_grad_norm(     # gradient clipping
+                engine.model.parameters(),
+                engine.config.max_grad_norm()
+            )
+            if engine.config.gpu_id >= 0 and engine.config.off_autocast:    # <---
+                engine.scaler.step(engine.optimizer)
+                engine.scaler.update()  
+            else:
+                engine.optimizer.step()
+
+        loss = float(loss/word_count)
+        ppl = np.exp(loss)
+
+        return {
+            'loss': loss,
+            'ppl': ppl}
+
+    @staticmethod
+    def validation(engine, mini_batch):
+        engine.model.eval()
+
+        with torch.no_grad():
+            device = next(engine.model.parameters()).device
+            mini_batch.src = (mini_batch.src[0].to(device), mini_batch.src[1])
+            mini_batch.tgt = (mini_batch.tgt[0].to(device), mini_batch.tgt[1])
+
+            x, y = mini_batch.src, mini_batch.tgt[0][:, 1:] # <BOS>제외 <---------확인
+
+            with autocast(not engine.config.off_autocast):
+                y_hat = engine.model(x, mini_batch.tgt[0][:, :-1])
+                loss = engine.crit(
+                    y_hat.contiguous().view(-1, y_hat.size(-1)),
+                    y.contiguous().view(-1)
+                )
+
+        word_count =  int(mini_batch.tgt[1].sum())
+        loss = float(loss/word_count)
+        ppl = np.exp(loss)
+
+        return {
+            'loss': loss,
+            'ppl': ppl
+        }
+
+    @staticmethod
+    def attach(
+        train_engine, validation_engine,
+        training_metric_names = ['loss', 'ppl'],
+        validation_metric_names = ['loss', 'ppl'],
+        verbose=VERBOSE_BATCH_WISE
+    ):
+        def attach_running_average(engine, metric_name):
+            RunningAverage(output_transform=lambda x: x[metric_name]).attach(
+                engine,
+                metric_name
+            )
+        for metric_name in training_metric_names:
+            attach_running_average(train_engine, metric_name)
+
+        if verbose >= VERBOSE_EPOCH_WISE:
+            @train_engine.on(Events.EPOCH_COMPLETED)
+            def print_train_logs(engine):
+                avg_loss = engine.state.metrics['loss']
+
+                print('Epoch {} - loss={:.4e} ppl={:.2f}'.format(
+                    engine.state.epoch,
+                    avg_loss,
+                    np.exp(avg_loss)
+                ))
+
+        for metric_name in validation_metric_names:
+            attach_running_average(validation_engine, metric_name)
+
+        if verbose >= VERBOSE_EPOCH_WISE:
+            @validation_engine.on(Events.EPOCH_COMPLETED)
+            def print_valid_logs(engine):
+                avg_loss = engine.state.metrics['loss']
+
+                print('Validation - loss{:.4e} ppl={:.2f} best_loss={:.4e} best_ppl={:.2f}'.format(
+                    avg_loss,
+                    np.exp(avg_loss),
+                    engine.best_loss,
+                    np.exp(engine.best_loss)
+                ))
+    
+    @staticmethod
+    def resume_training(engine, resume_epoch):
+        engine.state.iteration = (resume_epoch - 1) * len(engine.state.dataloader)
+        engine.state.epoch = (resume_epoch - 1)
+    
+    @staticmethod
+    def check_best(engine):
+        loss = float(engine.state.metrics['loss'])
+        if loss <= engine.best_loss:
+            engine.best_loss = loss
+
+    @staticmethod
+    def save_model(engine, train_engine, config, src_vocab, tgt_vocab):
+        avg_train_loss = train_engine.state.metrics['loss']
+        avg_valid_loss = engine.state.metrics['loss']
+
+        model_fn = config.model_fn.split('.')
+        model_fn = model_fn[:-1] + ['%02d' % train_engine.state.epoch,
+                                    '%.2f-%.2f' % (avg_train_loss,
+                                                   np.exp(avg_train_loss)),
+                                    '%.2f-%.2f' % (avg_valid_loss,
+                                                   np.exp(avg_valid_loss))] + [model_fn[-1]]
+        model_fn = '.'.join(model_fn)
+        
+        torch.save(
+            {
+                'model': engine.model.state_dict(),
+                'opt': train_engine.optimizer.state_dict(),
+                'config': config,
+                'src_vocab': src_vocab,
+                'tgt_vocab': tgt_vocab
+            }, model_fn
+        )
+
+class SingleTrainer():
+
+    def __init__(self, target_engine_class, config):
+        self.target_engine_class = target_engine_class
+        self.config = config
+
+    def train(
+        self,
+        model, crit, optimizer,
+        train_loader, valid_loader,
+        src_vocab, tgt_vocab,
+        n_epochs,
+        lr_scheduler=None
+    ):
+        train_engine = self.target_engine_class(
+            self.target_engine_class.train,     # 등록
+            model,
+            crit,
+            optimizer,
+            lr_scheduler,
+            self.config
+        )
+        validation_engine = self.target_engine_class(
+            self.target_engine_class.validate,  # 등록
+            model,
+            crit,
+            optimizer=None,
+            lr_scheduler=None,
+            config=self.config
+        )
+        self.target_engine_class.attach(
+            train_engine,
+            validation_engine,
+            verbose=self.config.verbose
+        )
+        def run_validation(engine, validation_engine, valid_loader):
+            validation_engine.run(valid_loader, max_epochs=1)
+
+            if engine.lr_scheduler is not None:
+                engine.lr_scheduler.step()
+        
+        train_engine.add_event_handler(
+            Events.EPOCH_COMPLETED,
+            run_validation,
+            validation_engine,
+            valid_loader
+        )
+        train_engine.add_event_handler(
+            Events.STARTED,
+            self.target_engine_class.resume_training,
+            self.config.init_epoch
+        )
+        validation_engine.add_event_handler(
+            Events.EPOCH_COMPLETED,
+            self.target_engine_class.save_model,
+            train_engine,
+            self.config,
+            src_vocab,
+            tgt_vocab
+        )
+        
+        train_engine.run(train_loader, max_epochs=n_epochs)
+
+        return model
